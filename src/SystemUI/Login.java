c0b4b1fe75c3b8caf434525ac1cabff8a573490a
kaitlinculligan <81047241+kaitlinculligan@users.noreply.github.com>
2021-12-03 14:34:27 -0700

    Implemented Login according to detail class diagram

--- src/SystemUI/Login.java
+++ src/SystemUI/Login.java
@@ -1,3 +1,41 @@
+package SystemUI;
+import java.util.*;
+import java.lang.*;
+
+
 class Login {
-    
+    private ArrayList<String> passwords;
+    private ArrayList<String> usernames;
+    public static Login onlyInstance;
+
+    public Login getOnlyInstance(){
+        if(onlyInstance == null){
+            onlyInstance = new Login();
+        }
+        return onlyInstance;
+    }
+
+    public void addUsername(String user){
+        Login instance = getOnlyInstance();
+        instance.getUsernames().add(user);
+    }
+
+    public void setUsername(int index, String newUser){
+        Login instance = getOnlyInstance();
+        instance.getUsernames().set(index, newUser);
+    }
+
+    public void removeUsername(String newUser){
+        Login instance = getOnlyInstance();
+        int index =  instance.getUsernames().indexOf(newUser);
+        instance.getUsernames().remove(index);
+    }
+
+    public ArrayList<String> getUsernames(){
+        return getOnlyInstance().usernames;
+    }
+    private Login(){
+        passwords = new ArrayList<>();
+        usernames = new ArrayList<>();
+    }
 }
