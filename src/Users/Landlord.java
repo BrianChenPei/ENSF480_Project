a79c370b464c05122621e0589581d3d21f972615
ancaicedo <77050076+ancaicedo@users.noreply.github.com>
2021-12-03 18:07:54 -0700

    brian

--- src/Users/Landlord.java
+++ src/Users/Landlord.java
@@ -1,5 +1,19 @@
 package Users;
 
 public class Landlord {
-    
+    private String name;
+    private String email;
+    private Arraylist<Property> rgedProperties;
+
+    public void registerProperty(Property p){
+
+    }
+
+    public void payFee(double money){
+
+    }
+
+    public void changeState(String houseID, String newState){
+        
+    }
 }
--- src/Users/Manager.java
+++ src/Users/Manager.java
@@ -1,5 +1,21 @@
 package Users;
 
 public class Manager {
-    
+    private String name;
+
+    public Report askReport(){
+
+    }
+
+    public void changeFee(double fee){
+
+    }
+
+    public void changeFeePeriod(int period){
+
+    }
+
+    public void changeState(String houseID, String newState){
+        
+    }
 }
