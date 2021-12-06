036e5c85bbc37dbd2092f76910d7f474a0219a47
zheng-628 <zheng.chen1@ucalgary.ca>
2021-12-05 17:03:28 -0700

    add controller method

--- src/PRMS/PRMS.java
+++ src/PRMS/PRMS.java
@@ -1,5 +1,54 @@
 package PRMS;
 
+import Users.*;
+import Property.*;
+import Database.Database;
+import java.util.ArrayList;
+
 public class PRMS {
+
+    //Renterr's controller function
+    public ArrayList<Property> searchProperty(String searchCri){
+        
+    }
+    public void sendEmail (Property p){
     
+    }
+
+    public void subscribe(String searchCri){
+
+    }
+
+    public void unsubscribe(){
+        
+    }
+
+
+
+    //Manger's controller function
+    public void askReport(){
+        
+    }
+
+    public void changeFee(double fee){
+
+    }
+
+    public void changeFeePeriod(int period){
+
+    }
+
+    public void MchangeState(String houseID, String newState){
+
+    }
+
+    //Landlord's controller function
+
+    public void payFee(double money){
+
+    }
+
+    public void LchangeState(String houseID, String newState){
+
+    }
 }
