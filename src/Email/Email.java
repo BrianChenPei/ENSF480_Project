ccd436fdd19344265428761abb49d04872198db0
BrianChenPei <93567892+BrianChenPei@users.noreply.github.com>
2021-12-07 17:47:19 -0700

    added email class and account class

--- src/Email/Email.java
+++ src/Email/Email.java
@@ -1,5 +1,26 @@
 package Email;
 
 public class Email {
-    
+    private String message;
+	private String emailAddress;
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public void setEmail(String email) {
+		this.emailAddress = email;
+	}
+	
+	public String getMessage() {
+		return this.message;
+	}
+	
+	public String getEmailAddress() {
+		return this.emailAddress;
+	}
+	
+	public void sendTo() {
+		// not completed
+	}
 }
--- src/PRMS/Account.java
+++ src/PRMS/Account.java
@@ -1,5 +1,31 @@
 package PRMS;
 
 public class Account {
-    
+    private String usernamel
+	private String password;
+	private String userType;
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+	
+	public String getUsername() {
+		return this.username;
+	}
+	
+	public String getPassword() {
+		return this.password;
+	}
+	
+	public String getUserType() {
+		return this.userType;
+	}
 }
