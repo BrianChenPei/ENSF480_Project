ae323a484b783426fa0311ea1e2e63b15f16f004
kaitlinculligan <81047241+kaitlinculligan@users.noreply.github.com>
2021-12-10 19:28:17 -0700

    removed line 59 to resolve compile error

--- src/SystemUI/GUI.java
+++ src/SystemUI/GUI.java
@@ -56,7 +56,6 @@ public class GUI extends JFrame{
         createSearchPanel();
         createLandlordPanel();
         createManagerPanel();
-        createNotificationPanel();
         createFrame();
     }
 
